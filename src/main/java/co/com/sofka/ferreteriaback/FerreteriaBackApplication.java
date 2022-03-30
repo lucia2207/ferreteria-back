f462c504d448fa2ac0272a743448bf4a1b4975ac
lucia2207 <ludef15@gmail.com>
2022-03-30 12:11:58 -0300

    Dependencias agregadas

---
+++ src/main/java/co/com/sofka/ferreteriaback/FerreteriaBackApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.ferreteriaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/ferreteriaback/FerreteriaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.ferreteriaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerreteriaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
